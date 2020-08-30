a5d28028c742ca2e0cd29e73f4b9b8371cb132c6
Billy Williams <billysbunch72@gmail.com>
2020-08-30 12:30:22 -0600

    start over

---
+++ js/home.js
@@ -0,0 +1,8 @@
+function showMessage(message) {
+  document.getElementById("message").textContent = message;
+}
+showMessage("$15.00");
+let price = "20.00",
+  name = "purple sassy",
+  discounted = name;
+showMessage(discounted);
