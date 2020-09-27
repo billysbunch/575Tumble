746a84b1619dba51e78bace43c8122a74b67e355
Billy Williams <billysbunch72@gmail.com>
2020-09-27 06:59:13 -0600

    trying to put in a better calander

--- js/home.js
+++
@@ -1,8 +0,0 @@
-function showMessage(message) {
-  document.getElementById("message").textContent = message;
-}
-showMessage("$15.00");
-let price = "20.00",
-  name = "15.00",
-  discounted = name;
-showMessage(discounted);
